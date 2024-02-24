fd49e6c8604a41c4e8ac4ac5e88310ffd9036f5e
Fernanda Fernandes <32338349-fernandaferna20@users.noreply.replit.com>
2024-02-24 12:01:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const ano = 1991;
+let atrasoEmSegundos = 0.00016;
+let area= (16*3.14);
+let metadeArea = area/2;
+
+console.log("Ano: " + ano);
+console.log("Atraso em Segundos: " + atrasoEmSegundos);
+console.log("Área: " + area);
+console.log("Tipo: " + typeof metadeArea);
+
+console.log("------------------")
+
+let a = 10;
+let b = 0x10; 
+let c = 0o10;
+let d = 0b10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.log("------------------")
+
+let x = 9e3;
+let y = 123e-5;
+
+console.log(x);
+console.log(y);
+
+console.log("------------------")
+
+let z = 0x21;
+console.log(z);
+
+console.log("------------------")
+
+let pais = "Brasil";
+let continente = "América do Sul";
+
+console.log(pais);
+console.log(typeof pais);
+console.log(continente);
+console.log(typeof continente);
+
+let message1 = "O návio 'Mars' fez escala no porto.";
+let message2 = 'Chuva forte passará perto da cidade de São Paulo.'
+
+console.log(message1);
+console.log(message2);
+
+console.log("------------------")
